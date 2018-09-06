97ce18d96aad9c48bad225c3721839af0a2c61ec
Rishi74744 <rm2998@gmail.com>
2018-09-06 13:16:57 +0530

    Initial Commit

---
+++ videotogif-auto-configure/src/main/java/com/spring/example/videotogif/autoconfigure/VideotogifAutoConfigureApplication.java
@@ -0,0 +1,12 @@
+package com.spring.example.videotogif.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotogifAutoConfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotogifAutoConfigureApplication.class, args);
+	}
+}
